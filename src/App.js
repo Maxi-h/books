7cab33086f341fd3254e13d306816311cb6ff1b4
maxi <maxi@maxi.maxi>
2021-06-22 00:34:47 -0500

    Books

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import { useState, useEffect, useContext, createContext } from 'react';
 import './App.css';
+import axios from 'axios';
+
+const Context = createContext({});
+
+function ContextProvider({ children }) {
+  const [contextProvider, setContextProvider] = useState({text: "java"})
+
+  return <Context.Provider value={{contextProvider, setContextProvider}}>
+    {children}
+  </Context.Provider>
+}
+
+
+function Content() {
+  const [data, setData] = useState(null);
+  const { contextProvider, setContextProvider } = useContext(Context)
+
+  useEffect(() => {
+
+    async function searchBook() {
+      const response = await axios(`https://hn.algolia.com/api/v1/search?query=${contextProvider.text}`)
+      setData(response.data)
+    }
+    searchBook()
+
+  }, [contextProvider])
+
+
+  return (
+
+    <div className="App">
+      <h1>Libros</h1>
+      <input type="text" value={contextProvider.text} onChange={e => {
+        setContextProvider({text: e.target.value} )
+      }}></input>
+      <ul>
+        {data && data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+  );
+}
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContextProvider>
+      <Content />
+    </ContextProvider>
   );
 }
 
